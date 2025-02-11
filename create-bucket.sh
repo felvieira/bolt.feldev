a4bccdb2d00a1c8e9b9ea0bdc479850a2a6e92ee
Diego Siqueira <130410307+dirgocs@users.noreply.github.com>
2025-02-11 11:11:59 -0400

    Delete create-bucket.sh

--- create-bucket.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Configurando bucket no Minio..."
-/usr/bin/mc alias set supabase-minio http://supabase-minio:9000 ${SERVICE_USER_MINIO} ${SERVICE_PASSWORD_MINIO}
-/usr/bin/mc mb --ignore-existing supabase-minio/bolt 
-echo "Bucket criado (se não existia)!"
