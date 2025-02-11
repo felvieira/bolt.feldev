75f50002cc0405cd59fd018a93c10da503ea7b9f
Diego Siqueira <130410307+dirgocs@users.noreply.github.com>
2025-02-10 23:29:48 -0400

    Create session.server.ts

---
+++ app/session.server.ts
@@ -0,0 +1,22 @@
+// app/session.server.ts
+import { createCookieSessionStorage } from '@remix-run/node';
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: '__session',
+    httpOnly: true,
+    path: '/',
+    sameSite: 'lax',
+    secrets: [sessionSecret],
+    secure: process.env.NODE_ENV === 'production',
+  },
+});
+
+export const getSession = (cookie: string | null) => sessionStorage.getSession(cookie);
+export const commitSession = (session: any) => sessionStorage.commitSession(session);
+export const destroySession = (session: any) => sessionStorage.destroySession(session);
