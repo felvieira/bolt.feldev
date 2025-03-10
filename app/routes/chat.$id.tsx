d8d405ad4b42c3ba45d4549b9159563fde5dc1e3
Diego Siqueira <130410307+dirgocs@users.noreply.github.com>
2025-03-10 14:32:20 -0400

    Update chat.$id.tsx

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,8 +1,12 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { Request, Response } from 'express';
 import { default as IndexRoute } from './_index';
+import { createApiHandler } from '~/utils/api-utils.server';
+import type { ExpressAppContext } from '~/utils/express-context-adapter.server';
 
-export async function loader(args: LoaderFunctionArgs) {
-  return json({ id: args.params.id });
-}
+export const loader = createApiHandler(async (context: ExpressAppContext, request: Request, response: Response) => {
+  const id = request.params.id;
+  response.status(200).json({ id });
+  return response;
+});
 
 export default IndexRoute;
