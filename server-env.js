ab0647d92f9b2c77c77ad2fdd6890bda8ed41bae
Diego Siqueira <130410307+dirgocs@users.noreply.github.com>
2025-03-10 13:51:33 -0400

    Delete server-env.js

--- server-env.js
+++
@@ -1,6 +0,0 @@
-// server-env.js
-console.log('Setting up server environment variables');
-
-// Define as variáveis de ambiente de forma global
-globalThis.HARDCODED_URL = 'INJECTED_SUPABASE_URL';
-globalThis.HARDCODED_KEY = 'INJECTED_SUPABASE_KEY';
