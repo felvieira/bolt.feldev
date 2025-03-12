b7764bf0d7671bde1ebd90805b59390391db8329
Diego Siqueira <130410307+dirgocs@users.noreply.github.com>
2025-03-11 20:13:05 -0400

    Update chat.$id.tsx

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express';
 import { default as IndexRoute } from './_index';
-import { createApiHandler } from '~/utils/api-utils.server';
 import type { ExpressAppContext } from '~/utils/express-context-adapter.server';
-import { json } from '@remix-run/node';
 
-export const loader = createApiHandler(async (context: ExpressAppContext, request: Request, response: Response) => {
-  const id = request.params.id;
-  return json({ id });
-});
+export const loader = async (args: { context: ExpressAppContext, request: Request }) => {
+  const { json } = await import('@remix-run/node');
+  const { createApiHandler } = await import('~/utils/api-utils.server');
+  
+  const handler = createApiHandler(async (context: ExpressAppContext, request: Request, response: Response) => {
+    const id = request.params.id;
+    return json({ id });
+  });
+
+  return handler(args.context, args.request, args.context.res);
+};
 
 export default IndexRoute;
